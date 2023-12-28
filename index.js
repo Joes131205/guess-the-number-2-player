94f9656d002c53fbd4637da22c330813be962f67
Joes131205 <Joes131205@users.noreply.github.com>
2023-12-28 22:00:16 +0700

    wth is this commit message lol
    Refactor handling win condition
    Refactor the win condition handling function to improve user experience
    and display appropriate messages after the win, enhancing the game's
    interactivity. Additionally, modify the styles to ensure a clear visual
    indication of the winning state. No issue references.

--- index.js
+++ index.js
@@ -30,88 +30,94 @@ let playing = true;
 restartButton.addEventListener("click", startGame);
 
 function startGame() {
-    randomNumbers[0] = generateRandomNumber();
-    randomNumbers[1] = generateRandomNumber();
+  randomNumbers[0] = generateRandomNumber();
+  randomNumbers[1] = generateRandomNumber();
 
-    guesses[0] = 0;
-    guesses[1] = 0;
+  guesses[0] = 0;
+  guesses[1] = 0;
 
-    number1Input.value = "";
-    number2Input.value = "";
+  number1Input.value = "";
+  number2Input.value = "";
 
-    randomNumber1El.textContent = "?";
-    randomNumber2El.textContent = "?";
+  statusText1.textContent = "?";
+  statusText2.textContent = "?";
 
-    player2Container.classList.add("inactive");
-    player1Container.classList.remove("inactive");
+  guessesText1.textContent = 0;
+  guessesText2.textContent = 0;
+
+  randomNumber1El.textContent = "?";
+  randomNumber2El.textContent = "?";
+
+  player2Container.classList.add("inactive");
+  player1Container.classList.remove("inactive");
+
+  player2Container.classList.remove("winner");
+  player1Container.classList.remove("winner");
+
+  playing = true;
 }
 
 startGame();
 function generateRandomNumber() {
-    return Math.floor(Math.random() * 101);
+  return Math.floor(Math.random() * 101);
 }
 
 function switchPlayer() {
-    player1Container.classList.toggle("inactive");
-    player2Container.classList.toggle("inactive");
+  player1Container.classList.toggle("inactive");
+  player2Container.classList.toggle("inactive");
 }
-function handleWin(player, container) {
-    playing = false;
-    container.classList.add("winner");
+function handleWin(player, container, status) {
+  playing = false;
+  container.classList.add("winner");
+  status.textContent = "You got it!";
+  randomNumber1El.textContent = randomNumbers[0];
+  randomNumber2El.textContent = randomNumbers[1];
 }
-function handleGuess(
-    container,
-    currentPlayer,
-    input,
-    button,
-    status,
-    guessText,
-) {
-    if (playing) {
-        const guess = Number(input.value);
-        if (!guess || guess < 0 || guess > 100) {
-            button.style.background = "red";
-            setTimeout(() => {
-                button.style.background = "white";
-            }, 500);
-            return;
-        }
-
-        if (guess === randomNumbers[currentPlayer]) {
-            handleWin(currentPlayer, container);
-            return;
-        } else {
-            guesses[currentPlayer]++;
-            guessText.textContent = guesses[currentPlayer];
-            if (guess > randomNumbers[currentPlayer]) {
-                status.textContent = "Too High!";
-            } else if (guess < randomNumbers[currentPlayer]) {
-                status.textContent = "Too Low!";
-            }
-            currentPlayer = currentPlayer === 0 ? 1 : 0;
-            switchPlayer();
-        }
+function handleGuess(container, input, button, status, guessText) {
+  if (playing) {
+    console.log(randomNumbers);
+    const guess = Number(input.value);
+    if (!guess || guess < 0 || guess > 100) {
+      button.style.background = "red";
+      setTimeout(() => {
+        button.style.background = "white";
+      }, 500);
+      return;
     }
+    guesses[currentPlayer]++;
+    guessText.textContent = guesses[currentPlayer];
+    console.log(guess === randomNumbers[currentPlayer]);
+    if (guess === randomNumbers[currentPlayer]) {
+      handleWin(currentPlayer, container, status);
+      return;
+    } else {
+      if (guess > randomNumbers[currentPlayer]) {
+        status.textContent = "Too High!";
+      } else if (guess < randomNumbers[currentPlayer]) {
+        status.textContent = "Too Low!";
+      }
+      currentPlayer = currentPlayer === 0 ? 1 : 0;
+      switchPlayer();
+    }
+  }
 }
 
 guess1Button.addEventListener("click", function () {
-    handleGuess(
-        player1Container,
-        currentPlayer,
-        number1Input,
-        guess1Button,
-        statusText1,
-        guessesText1,
-    );
+  handleGuess(
+    player1Container,
+    number1Input,
+    guess1Button,
+    statusText1,
+    guessesText1
+  );
 });
 
 guess2Button.addEventListener("click", function () {
-    handleGuess(
-        player1Container,
-        currentPlayer,
-        number2Input,
-        guess2Button,
-        statusText2,
-        guessesText2,
-    );
+  handleGuess(
+    player2Container,
+    number2Input,
+    guess2Button,
+    statusText2,
+    guessesText2
+  );
 });
